72402c6d264de40d06f02cbf528d428b7f420a74
thutruongvn <thutruong.1986@gmail.com>
2019-07-03 23:59:44 -0500

    commit for JS-2 assignment

--- js/bankAcc.js
+++ js/bankAcc.js
@@ -1,22 +1,31 @@
-"use strict"
-var accountInfoList = [];
+"use strict";
+var _accountInfoList = [];
 const Module = (function() {
     const _addNewAccount = function(name, deposit) {
-        accountInfoList.push({
+        _accountInfoList.push({
             accountName: name,
             deposit
         });
     };
-    const addNewAccount = function(name, deposit) {
-        _addNewAccount(name, deposit);
+    const addNewAccount = function() {
+        let txtName = document.getElementById("txtAccountName");
+        let txtDeposit = document.getElementById("numDeposit");
+        _addNewAccount(txtName.value || "", txtDeposit.value || "");
+        var txtResult = document.getElementById("txtaResult");
+        var lstAccounts = _accountInfoList.map( (e, i, array)  => "Account name:" + e.accountName + " deposit:" + e.deposit );
+        txtResult.value = lstAccounts.join("\n");
+        txtName.value = "";
+        txtDeposit.value = "";
     };
 
     return {
         addNewAccount: addNewAccount
     };
 })();
-var btnAdd = document.getElementById("btnCreateAcc");
-btnAdd.onclick = Module.addNewAccount;
-var txtResult = document.getElementById("txtaResult");
-txtResult.innerText = accountInfoList.forEach( e => "Account name:" + e.accountName + " deposit:" + e.deposit);
+window.onload = function () {
+    var btnAdd = document.getElementById("btnCreateAcc");
+    btnAdd.onclick = Module.addNewAccount;
+    
+    
 
+}
