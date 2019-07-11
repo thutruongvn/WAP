0bcab72c3e3a2af0f6c6d4f27dfc3b987b4d473f
thutruongvn <thutruong.1986@gmail.com>
2019-07-10 20:52:42 -0500

    update maze

--- js/maze.js
+++ js/maze.js
@@ -1,8 +1,31 @@
 "use strict";
-
+var start = false;
 $( function() {
-    $("#boundary1").mouseover( () => {
-        let e = $(this);
-        e.addClass("youlose");
+    $("#maze .boundary").mouseover( () => {
+        // let e = $(this);
+        // console.log($("#boundary1"));
+        // e.css("background-color", "#ff8888");
+        // $("#boundary1").css("background-color", "#ff8888");
+        if(start) {
+            $(".boundary").addClass("youlose");
+        }
+
+    });
+
+    $("#start").click( () => {
+        $("#maze .boundary").removeClass("youlose");
+        start = true;
+    });
+
+    $("#end").mouseover( () => {
+        let e = $("#maze .youlose");
+        // console.log(e.length);
+        if (start) {
+            let txt = e.length > 0 ? "Sorry, you lost. :[" : "You win! :]";
+            $("#status").text(txt);
+            start = false;
+            // alert("Sorry, you lost. :[");
+            // alert("You win! :]")
+        } 
     });
 });
